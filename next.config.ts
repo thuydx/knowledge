d8c27885b78d139e5fd86b5deae35ccd1617f453
Thuy Dinh Xuan <thuy.dinh@hdwebsoft.genixventures.com>
2025-05-06 18:30:07 +0700

    init knowledge base

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,9 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   output: 'export',
   reactStrictMode: true,
+  sassOptions: {
+    quietDeps: true, // 👈 Suppress Sass warnings from node_modules
+  },
 };
 
 export default nextConfig;
