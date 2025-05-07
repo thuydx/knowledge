5b7c859221dfd5f56e18219eeb22a1843f9499b0
Thuy Dinh Xuan <thuy.dinh@hdwebsoft.genixventures.com>
2025-05-07 10:03:12 +0700

    update next config

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,20 @@
 import type { NextConfig } from "next";
+import type { Configuration, Compilation } from 'webpack';
 
 const nextConfig: NextConfig = {
   output: 'export',
   reactStrictMode: true,
   sassOptions: {
-    quietDeps: true, // 👈 Suppress Sass warnings from node_modules
+    quietDeps: true, // Suppress Sass warnings from node_modules
+  },
+  // Hide the deprecated warning of 3rd-party
+  webpack(config: Configuration) {
+    config.ignoreWarnings = [
+      (warning: Error, _compilation: Compilation) =>
+        typeof warning.message === 'string' &&
+        warning.message.includes('has been deprecated as of'),
+    ];
+    return config;
   },
 };
 
